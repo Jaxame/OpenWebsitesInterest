ff8db28602261d8677b307c7f4e3403720fb09a0
JACKY AME <jacky.ame@orange.fr>
2022-11-25 11:04:48 +0100

    initial main script

---
+++ open_website.py
@@ -0,0 +1,11 @@
+import webbrowser as wb
+
+# Display url using the default browser
+url = 'https://www.codebreakthrough.com/python-bootcamp/'
+wb.open(url)
+
+### Open a website in a new window with the firefox browser 
+# wb.get('firefox').open_new(url)
+
+### Open a website in a new page with the firefox browser 
+# wb.get('firefox').open_new_tab(url)
