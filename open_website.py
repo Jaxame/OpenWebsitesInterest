273cf1316dbeb1dc3d3c8ba6e32ec3e7fa86dff2
JACKY AME <jacky.ame@orange.fr>
2022-11-28 10:59:43 +0100

    test check_file_exists function

--- open_website.py
+++ open_website.py
@@ -2,15 +2,31 @@ from os.path import isfile
 import webbrowser as wb
 
 def check_file_exists(path: str) -> bool:
+    """
+    Check if file exists with a good path
+
+    Args:
+        path (str): Absolute path with name file to check
+
+    Raises:
+        FileExistsError: Error with the path or the name of the file
+
+    Returns:
+        bool: True if file exists, FileExistsError otherwise
+    """
     if isfile(path):
         print("The file exists")
         return True
     else:
-        return False
+        raise FileExistsError("It's not a file, we can't open it")
+
+# TESTING check_file_exists
+print(check_file_exists("/home/jacky/projects/OpenWebsitesInterest/list_websites.txt"))
+print(check_file_exists("/home/jacky/projects/OpenWebsitesInterest/list_websites.tx"))
 
 # Display url using the default browser
-url = 'https://www.codebreakthrough.com/python-bootcamp/'
-wb.open(url)
+# url = 'https://www.codebreakthrough.com/python-bootcamp/'
+# wb.open(url)
 
 ### Open a website in a new window with the firefox browser 
 # wb.get('firefox').open_new(url)
